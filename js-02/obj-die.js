0d5e8617ccfc1caf1a153e4fb07ba7d879f6f6c7
Tomxss <46247615+Tomxss@users.noreply.github.com>
2020-01-09 13:37:06 +0200

    Added roll function to object

--- js-02/obj-die.js
+++ js-02/obj-die.js
@@ -1,9 +1,11 @@
-var getDieRoll = function(dieSize = 6) {
-    return (Math.ceil(dieSize * Math.random()));
+var die = {
+    size: 4,
+    count: 1,
+    roll: function(dieSize = 6) {
+        return (Math.ceil(dieSize * Math.random()));
+    }
+
 };
 
-var die = {
-
-}
-
 console.log(die);
+console.log(die.roll());
