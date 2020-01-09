80f08caa9ea43ea98b3384b2f091f09c65403384
Tomxss <46247615+Tomxss@users.noreply.github.com>
2020-01-09 13:56:35 +0200

    Added

--- js-02/obj-die.js
+++ js-02/obj-die.js
@@ -1,11 +1,15 @@
 var die = {
-    size: 4,
-    count: 1,
-    roll: function(dieSize = 6) {
-        return (Math.ceil(dieSize * Math.random()));
+    size: 6,
+    totalRolls: 0,
+    roll: function() {
+        this.totalRolls++;
+        return (Math.ceil(this.size * Math.random()));
     }
 
 };
-
-console.log(die);
+die.size = 10;
 console.log(die.roll());
+console.log(die.roll());
+console.log(die.roll());
+console.log(die.totalRolls);
+console.log(die);
