f872958c70481eb8ec673415e974486ca6756db2
Tomxss <46247615+Tomxss@users.noreply.github.com>
2020-01-12 14:15:56 +0200

    Served up some html

--- WebAppsNodeExpress/app.js
+++ WebAppsNodeExpress/app.js
@@ -2,15 +2,16 @@ var express = require('express');
 var chalk = require('chalk'); //sets colours on our console messages.
 var debug = require('debug')('app'); //better way to send out error messages, easier to keep track of what goin on.
 var morgan = require('morgan');// morgan gives you http request info
+var path = require('path');
 
 var app = express();
 
 app.use(morgan('tiny'));//combined gives more info, tiny gives less
 
 app.get('/', function(req, res) {
-    res.send("Hello from my Library app");
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));  //__dirname is where the app.js exectuble lies.
 });
 
 app.listen(3000, function() {
     debug(`listening on port ${chalk.green('3000')}`);
-});
+}); 
